9d9f9821d66a38222a13d1eb47c59211f83fcc38
Isabelvitoriano <isabelvitorianopereira@gmail.com>
2026-09-22 16:56:10 -0300

    lançando o componente Header

--- frontend/gestaoEscolar/src/components/Header/Header.jsx
+++ frontend/gestaoEscolar/src/components/Header/Header.jsx
@@ -1,32 +1,54 @@
 import './Header.css'
-import Icon from '../../assets/icon.svg'
-import Resetar from '../../assets/resetar.svg'
+import Icon from '../../assets/icon.svg?react'
+import Resetar from '../../assets/resetar.svg?react'
+import Aluno from '../../assets/aluno.svg?react'
+import Curso from '../../assets/curso.svg?react'
+import Matricula from '../../assets/matricula.svg?react'
 
 function Header() {
+    
+    
 
   return (
     <>
         <header className='cabecalho'>
             <div className='icone'>
-                <img src={Icon} alt="icone de escola" />
+                <Icon className='icon'/>
             </div>
             
             <div className='titulo'>
                 <h1>DevSchool</h1>
-                <span>Sistema de Gerenciamento</span>
             </div>
 
-            <button type={Reiniciar}>
+            <button className='reiniciar'>
                 Reiniciar Sistema
-                <img src={Resetar} alt="Reinciar o sistema" />
+                <Resetar className='icon-reset'/>
+
             </button>
         </header>
+
+        <nav className='navbar'>
+            <button >
+                <Aluno className='icon-aluno'/>
+                <span>Alunos</span>
+                <span className='contador'>4</span>
+            </button>
+            
+            <button>
+                <Curso className='icon-curso'/>
+                <span>Cursos</span>
+                <span className='contador'>3</span>
+            </button>
+            
+            <button>
+                <Matricula className='icon-matricula'/>
+                <span>Matrículas</span>
+                <span className='contador'>4</span>
+            </button>
+        </nav>
     </>
   )
 }
 
-function Reiniciar() {
-
-}
 
 export default Header;
